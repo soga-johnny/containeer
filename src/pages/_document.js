672b14a382b554c775e989bac33f55669ff516d9
soga-johnny <soga.johnny@gmail.com>
2023-06-16 16:58:09 +0900

    addFavicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,10 @@ export default function Document() {
       {/* <Head /> */}
       <Head>
       <link rel="icon" href="/favicon.ico"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link 
       rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
