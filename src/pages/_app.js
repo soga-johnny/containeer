a0e4a7e8f7cc049e93cf7d455f152d3df50f057b
soga-johnny <soga.johnny@gmail.com>
2023-07-18 16:38:18 +0900

    addAnalytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function App({ Component, pageProps }) {
   return ( 
@@ -7,6 +9,7 @@ export default function App({ Component, pageProps }) {
   {/* <ThemeProvider attribute='class' suppressHydrationWarning> */}
 
   <Component {...pageProps} />
+  <Analytics />
   </ThemeProvider>
   )
 }
